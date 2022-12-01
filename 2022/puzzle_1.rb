eb776ac0606ee807e2b5048409c667313003aac3
Briar Bowser <briarbear7@gmail.com>
2022-12-01 13:26:59 -0600

    Add solution for 2022 puzzle 1.

---
+++ 2022/puzzle_1.rb
@@ -0,0 +1,8 @@
+calories_per_elf = File.open("puzzle_input_1.txt") do |f|
+    f.readlines(chomp: true)
+        .slice_when { |i, line| line.empty? }
+        .map { |groups| groups.map(&:to_i).sum }
+end
+
+most_calories = calories_per_elf.max
+calories_per_elf.sort.reverse.take(3).sum
